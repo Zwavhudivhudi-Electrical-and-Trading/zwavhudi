2a556987b17d238d58d95916749a844617baad92
mobius-crypt <mobiusndou@gmail.com>
2024-05-04 21:13:52 +0200

    added inventory control

--- src/routes/cart.py
+++ src/routes/cart.py
@@ -46,26 +46,30 @@ async def place_orders(user: User, product_id: str):
     order_quantity = request.form.get('quantity')
 
     if user.customer_id:
-        customer_details: CustomerDetails = await customer_controller.get_customer_details(customer_id=user.customer_id)
-        product_details: Product = await product_controller.get_product(product_id=product_id)
-        customer_name = f"{customer_details.full_names} {customer_details.surname}"
-        email = customer_details.email if customer_details.email else user.email
+        customer_details: CustomerDetails| None = await customer_controller.get_customer_details(customer_id=user.customer_id)
+        product_details: Product| None = await product_controller.get_product(product_id=product_id)
+        if customer_details:
+            customer_name = f"{customer_details.full_names} {customer_details.surname}"
+            email = customer_details.email if customer_details.email else user.email
 
-        temp_order = await customer_controller.get_temp_order(customer_id=user.customer_id)
-        if not temp_order:
-            temp_order = Order(customer_id=user.customer_id, customer_name=customer_name, email=email,
-                               phone=customer_details.contact_number, address_id=customer_details.delivery_address_id,
-                               items_ordered=[])
+            temp_order = await customer_controller.get_temp_order(customer_id=user.customer_id)
+            if not temp_order:
+                temp_order = Order(customer_id=user.customer_id, customer_name=customer_name, email=email,
+                                   phone=customer_details.contact_number, address_id=customer_details.delivery_address_id,
+                                   items_ordered=[])
 
-            saved_temp_order = await customer_controller.add_temp_order(order=temp_order)
+                saved_temp_order = await customer_controller.add_temp_order(order=temp_order)
 
-        order_item = OrderItem(order_id=temp_order.order_id, product_id=product_id,
-                               product_name=product_details.name, price=product_details.price,
-                               quantity=order_quantity)
+            order_item = OrderItem(order_id=temp_order.order_id, product_id=product_id,
+                                   product_name=product_details.name, price=product_details.price,
+                                   quantity=order_quantity)
 
-        add_items_ordered = await customer_controller.add_items_to_temp_order(customer_id=user.customer_id,
-                                                                              order_id=temp_order.order_id,
-                                                                              order_item=order_item)
+            add_items_ordered = await customer_controller.add_items_to_temp_order(customer_id=user.customer_id,
+                                                                                  order_id=temp_order.order_id,
+                                                                                  order_item=order_item)
+        else:
+            # TODO - redirect to customer details captcha
+            pass
 
     flash(message="Order Successfully Captured", category="success")
     return redirect(url_for('cart.get_cart'))
